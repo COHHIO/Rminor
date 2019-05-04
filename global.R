461b37dc64e83157f4ed26b304eb7348abef2813
Genelle Denzin <genelle@eyedarts.net>
2019-05-04 10:39:14 -0400

    removed parts since those are in Rminor_elevated

--- global.R
+++ global.R
@@ -11,10 +11,5 @@ load("data/Utilization.RData")
 
 load("data/QPR_SPDATs.RData")
 
-ProviderAverages <- SPDATsByProject %>%
-  filter(served_between(SPDATsByProject, ReportStart, ReportEnd)) %>%
-  select(EnrollmentID, ProjectName, ScoreAdjusted) %>%
-  group_by(ProjectName) %>%
-  summarise(AverageScore = round(mean(ScoreAdjusted), 1),
-            EnrollmentCount = n())
+
 
