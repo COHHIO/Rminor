00cde90269f5c8f2f108be76233473f23a89055c
kiadso <genelle@eyedarts.net>
2019-08-19 16:17:50 -0400

    wrote three headers, still more to do moved some outputs around to be more in order of how the app is

--- server.R
+++ server.R
@@ -17,7 +17,35 @@ function(input, output, session) {
   #  # input$LoSRegionSelect
   # cat("Length of Stay", input$LoSRegionSelect)
   # })
-  observeEvent(c(input$providerList), {
+    output$headerUtilization <- renderUI({
+      ReportEnd <- ymd(paste0(
+        substr(
+          input$utilizationSlider,
+          str_length(input$utilizationSlider) - 4,
+          str_length(input$utilizationSlider)
+        ),
+        substr(
+          input$utilizationSlider,
+          1,
+          str_length(input$utilizationSlider) - 5
+        ),
+        "01"
+      )) +
+        months(1) - 1
+      ReportStart <- floor_date(ymd(ReportEnd), unit = "month") -
+        years(1) +
+        months(1)
+      
+      ReportStart <- format.Date(ymd(ReportStart), "%B %d, %Y")
+      ReportEnd <- format.Date(ymd(ReportEnd), "%B %d, %Y")
+      
+      list(h2("Bed and Unit Utilization"),
+           h4(input$providerListUtilization),
+           h4(ReportStart, "-", ReportEnd))
+    })
+    
+    observeEvent(c(input$providerList), {
+    
     output$currentUnitUtilization <-
       if (nrow(Utilization %>%
                filter(
@@ -251,81 +279,6 @@ function(input, output, session) {
     
   })
   
-  output$SPDATScoresByCounty <-
-    renderPlot({
-      # ReportStart <- format.Date(mdy(paste0("01-01-", input$y)), "%m-%d-%Y")
-      ReportStart <- format.Date(ymd(paste0(
-        substr(input$spdatSlider, 1, 4),
-        "-01-01"
-      )), "%m-%d-%Y")
-      ReportEnd <- format.Date(mdy(paste0(
-        case_when(
-          substr(input$spdatSlider, 7, 7) == 1 ~ "03-31-",
-          substr(input$spdatSlider, 7, 7) == 2 ~ "06-30-",
-          substr(input$spdatSlider, 7, 7) == 3 ~ "09-30-",
-          substr(input$spdatSlider, 7, 7) == 4 ~ "12-31-"
-        ),
-        substr(input$spdatSlider, 1, 4)
-      )), "%m-%d-%Y")
-      # counting all households who were scored AND SERVED between the report dates
-      CountyAverageScores <- CountyData %>%
-        filter(served_between(CountyData,
-                              ReportStart,
-                              ReportEnd)) %>%
-        select(CountyServed, PersonalID, Score) %>%
-        distinct() %>%
-        group_by(CountyServed) %>%
-        summarise(AverageScore = round(mean(Score), 1),
-                  HHsLHinCounty = n())
-      # counting all households who ENTERED either RRH or PSH between the report dates
-      CountyHousedAverageScores <- SPDATsByProject %>%
-        filter(entered_between(SPDATsByProject,
-                               ReportStart,
-                               ReportEnd)) %>%
-        group_by(CountyServed) %>%
-        summarise(HousedAverageScore = round(mean(ScoreAdjusted), 1),
-                  HHsHousedInCounty = n())
-      # pulling in both averages for each county plus adding Region for grouping
-      Compare <-
-        full_join(CountyAverageScores,
-                  CountyHousedAverageScores,
-                  by = "CountyServed") %>%
-        arrange(CountyServed) %>%
-        left_join(., Regions, by = c("CountyServed" = "County")) %>%
-        filter(RegionName == input$regionList)
-      # the plot
-      ggplot(Compare, aes(x = CountyServed, y = AverageScore)) +
-        geom_point(size = 12, shape = 95) +
-        scale_y_continuous(limits = c(0, 17)) +
-        geom_point(
-          aes(y = HousedAverageScore),
-          size = 6,
-          shape = 17,
-          colour = "#56B4E9"
-        ) +
-        xlab("County Where Served") +
-        ylab("Average SPDAT Score") +
-        theme_light() +
-        theme(
-          plot.title = element_text(lineheight = 5, size = rel(1.8)),
-          axis.text.x = element_text(size = rel(1.8)),
-          axis.text.y = element_text(size = rel(1.8)),
-          plot.margin = margin(
-            t = 15,
-            r = 15,
-            b = 15,
-            l = 15
-          )
-        ) +
-        labs(
-          title = input$regionList,
-          subtitle = paste("Date Range:", ReportStart, "to", ReportEnd),
-          caption = "VI-SPDAT scores and household enrollment data comes from
-          the Ohio Balance of State CoC HMIS. Detail may be found at R minor
-          elevated."
-        )
-    })
-  
   output$bedPlot <-
     renderPlot({
       ReportEnd <- ymd(paste0(
@@ -413,6 +366,102 @@ function(input, output, session) {
           COHHIO HMIS team."
         )
       
+    })  
+  
+  output$headerQPRCommunityNeed <- renderUI({
+    ReportStart <- format.Date(ymd(paste0(
+      substr(input$spdatSlider, 1, 4),
+      "-01-01"
+    )), "%m-%d-%Y")
+    ReportEnd <- format.Date(mdy(paste0(
+      case_when(
+        substr(input$spdatSlider, 7, 7) == 1 ~ "03-31-",
+        substr(input$spdatSlider, 7, 7) == 2 ~ "06-30-",
+        substr(input$spdatSlider, 7, 7) == 3 ~ "09-30-",
+        substr(input$spdatSlider, 7, 7) == 4 ~ "12-31-"
+      ),
+      substr(input$spdatSlider, 1, 4)
+    )), "%m-%d-%Y")
+    
+    list(h2("Quarterly Performance Report"),
+         h3("Community Need"),
+         h4(input$regionList),
+         h4(ReportStart, "-", ReportEnd))
+  })  
+  
+  output$SPDATScoresByCounty <-
+    renderPlot({
+      # ReportStart <- format.Date(mdy(paste0("01-01-", input$y)), "%m-%d-%Y")
+      ReportStart <- format.Date(ymd(paste0(
+        substr(input$spdatSlider, 1, 4),
+        "-01-01"
+      )), "%m-%d-%Y")
+      ReportEnd <- format.Date(mdy(paste0(
+        case_when(
+          substr(input$spdatSlider, 7, 7) == 1 ~ "03-31-",
+          substr(input$spdatSlider, 7, 7) == 2 ~ "06-30-",
+          substr(input$spdatSlider, 7, 7) == 3 ~ "09-30-",
+          substr(input$spdatSlider, 7, 7) == 4 ~ "12-31-"
+        ),
+        substr(input$spdatSlider, 1, 4)
+      )), "%m-%d-%Y")
+      # counting all households who were scored AND SERVED between the report dates
+      CountyAverageScores <- CountyData %>%
+        filter(served_between(CountyData,
+                              ReportStart,
+                              ReportEnd)) %>%
+        select(CountyServed, PersonalID, Score) %>%
+        distinct() %>%
+        group_by(CountyServed) %>%
+        summarise(AverageScore = round(mean(Score), 1),
+                  HHsLHinCounty = n())
+      # counting all households who ENTERED either RRH or PSH between the report dates
+      CountyHousedAverageScores <- SPDATsByProject %>%
+        filter(entered_between(SPDATsByProject,
+                               ReportStart,
+                               ReportEnd)) %>%
+        group_by(CountyServed) %>%
+        summarise(HousedAverageScore = round(mean(ScoreAdjusted), 1),
+                  HHsHousedInCounty = n())
+      # pulling in both averages for each county plus adding Region for grouping
+      Compare <-
+        full_join(CountyAverageScores,
+                  CountyHousedAverageScores,
+                  by = "CountyServed") %>%
+        arrange(CountyServed) %>%
+        left_join(., Regions, by = c("CountyServed" = "County")) %>%
+        filter(RegionName == input$regionList)
+      # the plot
+      ggplot(Compare, aes(x = CountyServed, y = AverageScore)) +
+        geom_point(size = 12, shape = 95) +
+        scale_y_continuous(limits = c(0, 17)) +
+        geom_point(
+          aes(y = HousedAverageScore),
+          size = 6,
+          shape = 17,
+          colour = "#56B4E9"
+        ) +
+        xlab("County Where Served") +
+        ylab("Average SPDAT Score") +
+        theme_light() +
+        theme(
+          plot.title = element_text(lineheight = 5, size = rel(1.8)),
+          axis.text.x = element_text(size = rel(1.8)),
+          axis.text.y = element_text(size = rel(1.8)),
+          plot.margin = margin(
+            t = 15,
+            r = 15,
+            b = 15,
+            l = 15
+          )
+        ) +
+        labs(
+          title = input$regionList,
+          subtitle = paste("Date Range:", ReportStart, "to", ReportEnd),
+          caption = "VI-SPDAT scores and household enrollment data comes from
+          the Ohio Balance of State CoC HMIS. Detail may be found at R minor
+          elevated."
+        )
     })
   
   output$CountyScoresText <-
@@ -562,7 +611,32 @@ function(input, output, session) {
                      }
                    })
                })
- # QPR Exits to PH 
+ 
+  # QPR Exits to PH 
+  
+  output$headerQPRExitsToPH <- renderUI({
+    ReportStart <- format.Date(ymd(paste0(
+      substr(input$ExitsToPHSlider, 1, 4),
+      "-01-01"
+    )), "%m-%d-%Y")
+    ReportEnd <- format.Date(mdy(paste0(
+      case_when(
+        substr(input$ExitsToPHSlider, 7, 7) == 1 ~ "03-31-",
+        substr(input$ExitsToPHSlider, 7, 7) == 2 ~ "06-30-",
+        substr(input$ExitsToPHSlider, 7, 7) == 3 ~ "09-30-",
+        substr(input$ExitsToPHSlider, 7, 7) == 4 ~ "12-31-"
+      ),
+      substr(input$ExitsToPHSlider, 1, 4)
+    )), "%m-%d-%Y")
+    
+    x <- renderText(input$ExitsToPHRegionSelect)
+    
+    list(h2("Quarterly Performance Report"),
+         h3("Exits to Permanent Housing"),
+         h4(x),
+         h4(ReportStart, "-", ReportEnd))
+  })  
+  
   observeEvent(
     c(input$ExitsToPHRegionSelect, input$ExitsToPHSlider),
     {
