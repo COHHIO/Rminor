223f82e09d337d752a62b65a902238cde5a92640
kiadso <genelle@eyedarts.net>
2019-09-05 10:59:08 -0400

    filtered the zz'd providers out of the Provider Dashboard list

--- global.R
+++ global.R
@@ -42,7 +42,14 @@ choices_month <-
 
 choices_regions <- unique(Regions$RegionName)
 
-providers <- sort(validation$ProjectName) %>% unique() 
+providers <- validation %>%
+  filter(str_detect(ProjectName, "zz", negate = TRUE) == TRUE)
+# the sample() function was pulling in a zz'd provider in the Provider Dashboard
+# so I'm filtering out the zz'd providers because why would they ever need to
+# check their Provider Dashboard? they wouldn't.
+providers <- 
+  sort(providers$ProjectName) %>% 
+  unique() 
 
 choices_project_type <- c(
   "Emergency Shelter", 
