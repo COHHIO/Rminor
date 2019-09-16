846b0b1add3bf20cb9c94921de4aed66b67624ab
kiadso <genelle@eyedarts.net>
2019-09-16 15:14:09 -0400

    replaced the old scratch code for working out plots with the Utilization plot I wanted to replace with a plotly (to match the look of other reports in R minor).

--- plot_build.R
+++ plot_build.R
@@ -1,95 +1,99 @@
 library(tidyverse)
 library(lubridate)
 library(plotly)
+library(viridis)
 
-ReportStart <- "01012019"
-ReportEnd <- "03312019"
+ReportStart <- "20180101"
+ReportEnd <- "20181231"
+ReportingPeriod <- interval(ymd(ReportStart), ymd(ReportEnd))
 
-load("data/QPR_EEs.RData")
+Provider <- "Warren - Warren MHA - Transitions - TH"
 
-LoSGoals <- goals %>%
-  select(-Measure) %>%
-  filter(SummaryMeasure == "Length of Stay" &
-           !is.na(Goal)) %>%
-  unique()
+load("data/Utilization.RData")
 
-LoSDetail <- QPR_EEs %>%
-  filter((((!is.na(MoveInDateAdjust) &
-              ProjectType %in% c(13)) |
-             (ProjectType %in% c(1, 2, 8)) &
-             !is.na(ExitDate)
-  )) &
-    exited_between(., ReportStart, ReportEnd)) %>%
+bedPlot <- BedUtilization %>% select(-FilePeriod) %>%
+  gather("Month",
+         "Utilization",
+         -ProjectID,
+         -ProjectName,
+         -ProjectType) %>%
+  filter(ProjectName == Provider,
+         mdy(Month) %within% ReportingPeriod) %>%
   mutate(
-    ProjectType = case_when(
-      ProjectType == 1 ~ "Emergency Shelter",
-      ProjectType == 2 ~ "Transitional Housing",
-      ProjectType %in% c(3, 9) ~ "Permanent Supportive Housing",
-      ProjectType == 4 ~ "Street Outreach",
-      ProjectType == 8 ~ "Safe Haven",
-      ProjectType == 12 ~ "Homelessness Prevention",
-      ProjectType == 13 ~ "Rapid Rehousing"
-    ),
-    Region = paste("Homeless Planning Region", Region)
-  ) %>%
-  filter(Region == "Homeless Planning Region 5") # this filter needs
-# to be here so the selection text matches the mutated data
+    Month = floor_date(mdy(Month), unit = "month"),
+    Bed = Utilization,
+    Utilization = NULL
+  )
 
-LoSSummary <- LoSDetail %>%
-  group_by(brokenProjectNames,
-           FriendlyProjectName,
-           Region,
-           County,
-           ProjectType) %>%
-  summarise(Days = case_when(
-    "Average Days" == "Average Days" ~ 
-      as.integer(mean(DaysinProject, na.rm = TRUE)),
-    "Average Days" == "Median Days" ~
-      as.integer(median(DaysinProject, na.rm = TRUE))
-  ))
+unitPlot <- UnitUtilization %>% select(-FilePeriod) %>%
+  gather("Month",
+         "Utilization",
+         -ProjectID,
+         -ProjectName,
+         -ProjectType) %>%
+  filter(ProjectName == Provider,
+         mdy(Month) %within% ReportingPeriod) %>%
+  mutate(
+    Month = floor_date(mdy(Month), unit = "month"),
+    Unit = Utilization,
+    Utilization = NULL
+  )
 
-esdata <- LoSSummary %>% filter(ProjectType == "Emergency Shelter")
+utilizationPlot <- unitPlot %>%
+  full_join(bedPlot,
+            by = c("ProjectID", "ProjectName", "ProjectType", "Month")) 
 
-es <-
-  ggplot(
-    esdata,
-    aes(x = FriendlyProjectName)
+plot_ly(utilizationPlot, 
+        x = ~Month) %>%
+  add_trace(y = ~ Unit,
+            name = "Unit Utilization",
+            type = "scatter",
+            mode = "lines+markers",
+            hoverinfo = 'y') %>%
+  add_trace(y = ~Bed,
+            name = "Bed Utilization",
+            type = "scatter",
+            mode = "lines+markers",
+            hoverinfo = 'y') %>%
+  layout(yaxis = list(
+    title = "Utilization",
+    tickformat = "%",
+    range = c(0, 2)
+  ),
+  title = paste("Bed and Unit Utilization,",
+                Provider,
+                "\n", 
+                format(ymd(ReportStart), "%B %Y"), 
+                "to", 
+                format(ymd(ReportEnd), "%B %Y")))
+
+
+
+
+ggplot(utilizationPlot,
+       aes(x = Month,
+           y = Utilization,
+           color = UtilizationType)) +
+  theme_light() +
+  geom_line(size = 1) +
+  geom_point(size = 2) +
+  scale_y_continuous(limits = c(0, 2),
+                     labels = scales::percent_format(accuracy = 1)) +
+  scale_x_date(
+    date_labels = "%B %Y",
+    date_breaks = "3 months",
+    minor_breaks = "1 month"
   ) +
-  ggtitle("Emergency Shelter", subtitle = "date range") +
-  geom_col(aes(y = Days), fill = "#7156e9") +
-  ylab("Average Length of Stay") +
-  xlab("") +
-  # geom_hline(yintercept = as.integer(LoSGoals %>%
-  #                                      filter(ProjectType == 1) %>%
-  #                                      select(Goal))) +
-  # annotate(
-  #   "text",
-  #   x = 0.65,
-  #   y = as.integer(LoSGoals %>%
-  #                    filter(ProjectType == 1) %>%
-  #                    select(Goal)) + 1,
-  #   label = "CoC Goal"
-  # ) +
-theme_light() +
-  theme(axis.text.x = element_text(angle = 45))
-
-plot_ly(data = esdata,
-        x = ~FriendlyProjectName,
-        y = ~Days) %>%
-  add_trace(type = "bar") %>%
-  layout(shapes=list(type='line', 
-                     xref = "x",
-                     yref = "y",
-                     x0= -.5,
-                     # x1= 5.5,
-                     y0 = as.integer(LoSGoals %>%
-                       filter(ProjectType == 1) %>%
-                       select(Goal)),
-                     y1= as.integer(LoSGoals %>%
-                       filter(ProjectType == 1) %>%
-                       select(Goal)), 
-                     line=list(width=1)
-                     ),
-         title = 'Emergency Shelters',
-         yaxis = list(title = "Average Days", showgrid = TRUE),
-         xaxis = list(title = "Project Name", showgrid = TRUE))
+  scale_colour_manual(values = c("#56B4E9", "#6be956")) +
+  labs(
+    title = Provider,
+    subtitle = paste(
+      "Date Range:",
+      format.Date(ymd(ReportStart), "%b %Y"),
+      "to",
+      format.Date(ymd(ReportEnd), "%b %Y")
+    ),
+    caption = "Client and household enrollment data comes from the Ohio
+          Balance of State CoC HMIS. This visualization was created by the
+          COHHIO HMIS team."
+  )
