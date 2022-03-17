43b47ce1066fe770f76be72be628a3b4eb13b7d2
Trevin <twflick@gmail.com>
2022-03-17 10:43:21 -0400

    changes after review

--- R/mod_utilization.R
+++ R/mod_utilization.R
@@ -12,35 +12,14 @@ mod_utilization_ui <- function(id){
   ns <- shiny::NS(id)
   shiny::fluidPage(
     ui_header_row(),
-    ui_row(box(
-      pickerInput(
-        inputId = "providerListUtilization",
-        choices = c(sort(utilization_bed$ProjectName)),
-        options = pickerOptions(dropupAuto = FALSE,
-                                liveSearch = TRUE),   
-        selected = sample_n(utilization_bed |> select(ProjectName), 1),
-        width = "100%"
-      ),
-      airDatepickerInput(
-        inputId = "utilizationDate",
-        label = "Report End Month",
-        maxDate = ymd(floor_date(update_date, unit = "month") - 1),
-        minDate = ymd(floor_date(update_date - 335, unit = "month")),
-        dateFormat = "MM yyyy",
-        view = "month",
-        value = ymd(floor_date(update_date, unit = "month") - 1),
-        minView = "months",
-        addon = "none",
-        autoClose = TRUE,
-        width = '25%'
-      ),
-      width = 12
-    )), 
-    plotlyOutput("bedPlot"),
-    br(),
+    ui_row(ui_picker_program(choices = sort(utilization_bed()$ProjectName)),
+           ui_date_range(start = lubridate::floor_date(Sys.Date(), unit = "month") -
+                           lubridate::years(1) +
+                           lubridate::months(1))),
+    plotly::plotlyOutput(ns("bed_plot")),
+    htmltools::br(),
     ui_row(
       title = "Summary",
-      fluidRow(
         bs4Dash::column(4, bs4Dash::infoBoxOutput(ns(
           "infobox_bed"
         ), width = '100%')),
@@ -49,9 +28,7 @@ mod_utilization_ui <- function(id){
         ), width = '100%')),
         bs4Dash::column(4, bs4Dash::infoBoxOutput(ns(
           "infobox_utilization"
-        ), width = '100%'))
-      ),
-      width = 12
+        ), width = '100%')),
     )
   )
 }
@@ -60,7 +37,6 @@ mod_utilization_server <- function(input, output, session){
   ns <- session$ns
   
   output$header <- shiny::renderUI(server_header("Bed and Unit Utilization", 
-                                                 format(input$date_range, "%B %Y"), 
                                                  shiny::tags$p(shiny::icon("exclamation-triangle", 
                                                                            style = "display:inline-block; 
                                                                            color: #ffc107;"), 
@@ -70,62 +46,54 @@ mod_utilization_server <- function(input, output, session){
                                                                Please continue to keep your clients safe.", 
                                                                status = "warning")))
   
-  output$bedPlot <-
-    renderPlotly({
-      ReportEnd <- ymd(input$utilizationDate) 
-      ReportStart <- floor_date(ReportEnd, unit = "month") -
-        years(1) +
-        months(1)
-      ReportingPeriod <- interval(ReportStart, ReportEnd)
+  output$bed_plot <-
+    plotly::renderPlotly({
+      ReportingPeriod <- lubridate::interval(input$date_range[1], input$date_range[2])
       
-      Provider <- input$providerListUtilization
-      
-      bedPlot <- utilization_bed |> 
-        gather("Month",
+      bed_plot <- utilization_bed() |> 
+        tidyr::gather("Month",
                "Utilization",
                -ProjectID,
                -ProjectName,
                -ProjectType) |>
-        filter(ProjectName == Provider,
+        dplyr::filter(ProjectName == input$program,
                
-               mdy(Month) %within% ReportingPeriod) |>
-        mutate(
-          Month = floor_date(mdy(Month), unit = "month"),
+               Month %within% ReportingPeriod) |>
+        dplyr::mutate(
+          Month = lubridate::floor_date(Month, unit = "month"),
           Bed = Utilization,
           Utilization = NULL
         )
       
       unitPlot <- utilization_unit |> 
-        gather("Month",
+        tidyr::gather("Month",
                "Utilization",
                -ProjectID,
                -ProjectName,
                -ProjectType) %>%
-        filter(ProjectName == Provider,
-               mdy(Month) %within% ReportingPeriod) |>
-        mutate(
-          Month = floor_date(mdy(Month), unit = "month"),
+        dplyr::filter(ProjectName == input$program,
+               Month %within% ReportingPeriod) |>
+        dplyr::mutate(
+          Month = lubridate::floor_date(Month, unit = "month"),
           Unit = Utilization,
           Utilization = NULL
         )
       
-      utilizationPlot <- unitPlot |>
-        full_join(bedPlot,
-                  by = c("ProjectID", "ProjectName", "ProjectType", "Month")) 
-      
-      plot_ly(utilizationPlot, 
-              x = ~Month) |>
-        add_trace(y = ~ Unit,
+      unitPlot |>
+        dplyr::full_join(bed_plot,
+                  by = c("ProjectID", "ProjectName", "ProjectType", "Month")) |>
+        plotly::plot_ly(x = ~Month) |>
+        plotly::add_trace(y = ~ Unit,
                   name = "Unit Utilization",
                   type = "scatter",
                   mode = "lines+markers",
                   hoverinfo = 'y') |>
-        add_trace(y = ~Bed,
+        plotly::add_trace(y = ~Bed,
                   name = "Bed Utilization",
                   type = "scatter",
                   mode = "lines+markers",
                   hoverinfo = 'y') |>
-        layout(yaxis = list(
+        plotly::layout(yaxis = list(
           title = "Utilization",
           tickformat = "%",
           range = c(0, 2)
@@ -135,13 +103,41 @@ mod_utilization_server <- function(input, output, session){
         ),
         title = paste("Bed and Unit Utilization",
                       "\n", 
-                      Provider,
+                      input$program,
                       "\n", 
-                      format(ymd(ReportStart), "%B %Y"), 
+                      input$date_range[1], 
                       "to", 
-                      format(ymd(ReportEnd), "%B %Y")))
+                      input$date_range[2]))
       
     })
+  
+  output$current_bed_utilization <-
+    if (nrow(utilization() %>%
+             dplyr::filter(
+               ProjectName == input$providerList &
+               ProjectType %in% c(1:3, 8, 9)
+             )) > 0) {
+      shinydashboard::renderInfoBox({
+        shinydashboard::infoBox(
+          title = "Current Bed Utilization",
+          subtitle = paste(
+            utilization() %>%
+              dplyr::filter(ProjectName == input$providerList) %>%
+              dplyr::select(Clients),
+            "Clients in",
+            utilization() %>%
+              dplyr::filter(ProjectName == input$providerList) %>%
+              dplyr::select(BedCount),
+            "Beds"
+          ),
+          color = "purple",
+          icon = shiny::icon("bed"),
+          utilization() %>%
+            dplyr::filter(ProjectName == input$providerList) %>%
+            dplyr::select(BedUtilization)
+        )
+      })
+    }
 }
     
 ## To be copied in the UI
