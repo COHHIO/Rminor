45b69f97f81ac794e3a9cf82d9cf4bd54013a625
Trevin Flickinger <twflick@gmail.com>
2024-06-05 16:29:56 -0400

    add second plot to dashboard

--- R/mod_body_performance_summary.R
+++ R/mod_body_performance_summary.R
@@ -22,9 +22,16 @@ mod_body_performance_summary_ui <- function(id){
                         )
                       )
              ),
-             tabPanel("Tab 2",
-                      h3("Content for Tab 2"),
-                      p("This is where the content for Tab 2 goes.")
+             tabPanel("Exits to Permanent Housing",
+                      h3("Exits Summary"),
+                      tagList(
+                        ui_header_row(),
+                        plotly::plotlyOutput(ns("ps_plot_2")),
+                        ui_row(
+                          DT::dataTableOutput(ns("ps_table_2")),
+                          title = "Measure 2: Exits"
+                        )
+                      )
              ),
              tabPanel("Tab 3",
                       h3("Content for Tab 3"),
@@ -42,12 +49,8 @@ mod_body_performance_summary_ui <- function(id){
 mod_body_performance_summary_server <- function(id){
   moduleServer( id, function(input, output, session){
     ns <- session$ns
-    output$header <- renderUI({
-      server_header(
-        "Performance Summary"
-      )
-    })
-      
+    
+    # Length of Time
     output$ps_plot_1 <-
       plotly::renderPlotly({
         length_of_stay <- qpr_leavers() |> 
@@ -79,34 +82,64 @@ mod_body_performance_summary_server <- function(id){
           datatable_default()
       })
       
-      output$ps_plot_1 <-
+      # Exits to Permanent Housing
+      qpr_leavers <- qpr_leavers()
+      .exited <- qpr_leavers |> 
+        HMIS::exited_between(as.Date("2023-01-01"), as.Date("2023-12-31"), lgl = TRUE)
+      .served <- qpr_leavers |> 
+        HMIS::served_between(as.Date("2023-01-01"), as.Date("2023-12-31"), lgl = TRUE)
+      
+      .psh_hp <- qpr_leavers$ProjectType %in% c(3, 9, 12)
+      .es_th_sh_out_rrh <- qpr_leavers$ProjectType %in% c(0, 1, 2, 4, 8, 13)
+      
+      SuccessfullyPlaced <- dplyr::filter(qpr_leavers,
+                                          ((ProjectType %in% c(3, 9, 13) &
+                                              !is.na(MoveInDateAdjust)) |
+                                             ProjectType %in% c(0, 1, 2, 4, 8, 12)
+                                          ) &
+                                            # excluding non-mover-inners
+                                            (((DestinationGroup == "Permanent" |
+                                                 #exited to ph or still in PSH/HP
+                                                 is.na(ExitDate)) &
+                                                .psh_hp & # PSH & HP
+                                                .served
+                                            ) |
+                                              (
+                                                DestinationGroup == "Permanent" & # exited to ph
+                                                  .es_th_sh_out_rrh &
+                                                  .exited
+                                              )
+                                            ))
+      
+      # calculating the total households to compare successful placements to
+      TotalHHsSuccessfulPlacement <- 
+        dplyr::filter(qpr_leavers,
+                      (.served & .psh_hp) # PSH & HP
+                      |
+                        (.exited & .es_th_sh_out_rrh) # ES, TH, SH, OUT, RRH
+        )
+      
+      TotalCount <- TotalHHsSuccessfulPlacement |> 
+        dplyr::count(ProjectName) |> 
+        dplyr::rename(clients = n)
+      
+      SuccessCount <- SuccessfullyPlaced |>
+        dplyr::count(ProjectName) |>
+        dplyr::rename(success_clients = n)
+      
+      exits <- TotalCount |> 
+        dplyr::left_join(SuccessCount, by = "ProjectName") |>
+        {\(.) {replace(.,is.na(.),0)}}() |>
+        dplyr::mutate(Average = success_clients/clients)
+      
+      output$ps_plot_2 <-
         plotly::renderPlotly({
-          length_of_stay <- qpr_leavers() |> 
-            HMIS::exited_between(as.Date("2023-01-01"), as.Date("2023-12-31")) |> 
-            dplyr::filter(((
-              !is.na(MoveInDateAdjust) & ProjectType == 13
-            ) |
-              (
-                !is.na(ExitDate) & ProjectType %in% c(0, 1, 2, 8)
-              ))) |> 
-            dplyr::group_by(ProjectName) |>
-            dplyr::summarise(Average = round(mean(DaysinProject), 1),
-                             Median = median(DaysinProject), clients = dplyr::n(), .groups = "drop_last")
-          qpr_plotly(length_of_stay, "Length of Stay", hover = hover)
+            qpr_plotly(exits, "Exits to Permanent Housing", hover = hover)
         })
       
       
-      output$ps_table_1 <- DT::renderDT(server = FALSE, {
-        qpr_leavers() |> 
-          HMIS::exited_between(as.Date("2023-01-01"), as.Date("2023-12-31")) |> 
-          dplyr::filter(((
-            !is.na(MoveInDateAdjust) & ProjectType == 13
-          ) |
-            (
-              !is.na(ExitDate) & ProjectType %in% c(0, 1, 2, 8)
-            ))) |> dplyr::group_by(ProjectName) |>
-          dplyr::summarise(Average = round(mean(DaysinProject), 1),
-                           Median = median(DaysinProject), clients = dplyr::n(), .groups = "drop_last") |> 
+      output$ps_table_2 <- DT::renderDT(server = FALSE, {
+        exits |> 
           datatable_default()
       })
       
