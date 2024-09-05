f5c58a1d5d3631d9f6e75b28184cb7db7c00562f
Trevin Flickinger <twflick@gmail.com>
2024-09-05 16:46:41 -0400

    add temp permanent housing

--- R/qpr_expr.R
+++ R/qpr_expr.R
@@ -582,5 +582,111 @@ qpr_expr <- list(
       ) |> 
         DT::datatable(escape = FALSE)
     })
+  ),
+  #### Temp Permanent Housing
+  temp_permanent_housing = list(
+    expr = rlang::expr({
+      req(input$date_range, input$region)
+      
+      .by_region <- qpr_leavers() |> 
+        dplyr::filter(ProjectName == input$region)
+      .exited <- .by_region |> 
+        HMIS::exited_between(input$date_range[1], input$date_range[2], lgl = TRUE)
+      .served <- .by_region |> 
+        HMIS::served_between(input$date_range[1], input$date_range[2], lgl = TRUE)
+      
+      .so <- .by_region$ProjectType %in% c(4)
+      
+      SuccessfullyPlaced <- dplyr::filter(.by_region,
+                                          ((DestinationGroup == "Permanent" | DestinationGroup == "Temporary") &
+                                             .so & .exited))
+      
+      # calculating the total households to compare successful placements to
+      TotalHHsSuccessfulPlacement <- 
+        dplyr::filter(.by_region,
+                      (.exited & .so)
+        )
+      list(SuccessfullyPlaced = SuccessfullyPlaced,
+           TotalHHsSuccessfulPlacement = TotalHHsSuccessfulPlacement)
+      
+    }),
+    infobox = rlang::expr({
+      req(data_env())
+      
+      qpr_infobox(
+        data_env(),
+        title = "Successfully Placed",
+        color = "info",
+        value = scales::percent(nrow(.data$SuccessfullyPlaced) / nrow(.data$TotalHHsSuccessfulPlacement)),
+        icon = shiny::icon("key"),
+        subtitle = paste(
+          nrow(.data$SuccessfullyPlaced),
+          "/",
+          nrow(.data$TotalHHsSuccessfulPlacement),
+          "households"
+        )
+      )
+    }),
+    details = rlang::expr({
+      tibble::tibble(
+        ProjectType = c("Street Outreach"),
+        Goal = c("At least 60% of households in Outreach projects will move from unsheltered locations to temporary or permanent housing at exit"),
+        HowCalculated = c("Number of households who moved from unsheltered locations to temporary (ES or TH) or to PH upon exit / number of households who moved from unsheltered locations to any destination at exit")
+      ) |>
+        DT::datatable(escape = FALSE)
+    })
+  ),
+  #### Temp or Permanent Housing Youth
+  temp_permanent_housing_youth = list(
+    expr = rlang::expr({
+      req(input$date_range, input$region)
+      
+      .by_region <- qpr_leavers() |> 
+        dplyr::filter(ProjectName == input$region)
+      .exited <- .by_region |> 
+        HMIS::exited_between(input$date_range[1], input$date_range[2], lgl = TRUE)
+      .served <- .by_region |> 
+        HMIS::served_between(input$date_range[1], input$date_range[2], lgl = TRUE)
+      
+      .so <- .by_region$ProjectType %in% c(4)
+      
+      SuccessfullyPlaced <- dplyr::filter(.by_region,
+                                          ((DestinationGroup == "Permanent" | DestinationGroup == "Temporary") &
+                                             .so & .exited))
+      
+      # calculating the total households to compare successful placements to
+      TotalHHsSuccessfulPlacement <- 
+        dplyr::filter(.by_region,
+                      (.exited & .so)
+        )
+      list(SuccessfullyPlaced = SuccessfullyPlaced,
+           TotalHHsSuccessfulPlacement = TotalHHsSuccessfulPlacement)
+      
+    }),
+    infobox = rlang::expr({
+      req(data_env())
+      
+      qpr_infobox(
+        data_env(),
+        title = "Successfully Placed",
+        color = "info",
+        value = scales::percent(nrow(.data$SuccessfullyPlaced) / nrow(.data$TotalHHsSuccessfulPlacement)),
+        icon = shiny::icon("key"),
+        subtitle = paste(
+          nrow(.data$SuccessfullyPlaced),
+          "/",
+          nrow(.data$TotalHHsSuccessfulPlacement),
+          "households"
+        )
+      )
+    }),
+    rlang::expr({
+      tibble::tibble(
+        ProjectType = c("Street Outreach"),
+        Goal = c("At least XX% of households in Youth Outreach projects will move from unsheltered locations to temporary or permanent housing at exit"),
+        HowCalculated = c("Number of households who moved from unsheltered locations to temporary (ES or TH) or to PH upon exit / number of households who moved from unsheltered locations to any destination at exit")
+      ) |>
+        DT::datatable(escape = FALSE)
+    })
   )
 )
