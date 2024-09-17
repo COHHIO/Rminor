ca6bb5ab2648033573667b3782e1f2303d3e3a3c
Trevin Flickinger <twflick@gmail.com>
2024-09-17 10:15:26 -0400

    correctly get youth id

--- R/mod_body_qpr.R
+++ R/mod_body_qpr.R
@@ -1,7 +1,18 @@
-mod_body_qpr_ui <- function(id, choices = NULL, date_choices = NULL, ns = rlang::caller_env()$ns) {
+mod_body_qpr_ui <- function(id, choices = NULL, date_choices = NULL, ns = rlang::caller_env()$ns,
+                            is_youth = FALSE) {
   ns <- NS(id)
+
+  .id <- strip_id(id, is_youth = is_youth)  # Ensures that the ID is correctly set up
   
-  .id <- strip_id(id)  # Ensures that the ID is correctly set up
+  
+  # Adjust choices based on is_youth flag
+  region_choices <- if (is_youth) {
+    qpr_tab_choices[[paste0(.id, "_youth")]]$choices  # Youth-specific choices
+  } else {
+    qpr_tab_choices[[.id]]$choices  # Standard choices
+  }
+  
+  
   .defaults <- purrr::compact(list(
     Dates = list(
       inputId = ns("date_range"),
@@ -10,12 +21,12 @@ mod_body_qpr_ui <- function(id, choices = NULL, date_choices = NULL, ns = rlang:
     ),
     Regions = list(
       inputId = ns("region"),
-      choices = qpr_tab_choices[[.id]]$choices,  # Default to the first choice
-      selected = qpr_tab_choices[[.id]]$choices[1],
+      choices = region_choices,  # Default to the first choice
+      selected = region_choices[1],
       multiple = FALSE
     )
   ))
-  
+
   shiny::tagList(
     ui_header_row(ns("header")),
     ui_row(
@@ -47,7 +58,7 @@ mod_body_qpr_ui <- function(id, choices = NULL, date_choices = NULL, ns = rlang:
 }
 
 
-mod_body_qpr_server <- function(id, header, calculate_expr, infobox_expr, details_expr) {
+mod_body_qpr_server <- function(id, header, calculate_expr, infobox_expr, details_expr, is_youth = FALSE) {
   moduleServer(id, function(input, output, session) {
     ns <- session$ns
     
@@ -56,7 +67,6 @@ mod_body_qpr_server <- function(id, header, calculate_expr, infobox_expr, detail
       input$date_range
       input$region
     }, {
-      message("Inputs available: date_range = ", input$date_range, ", region = ", input$region)
       req(input$date_range, input$region)  # Ensure inputs are available
       
       # Header
@@ -67,11 +77,13 @@ mod_body_qpr_server <- function(id, header, calculate_expr, infobox_expr, detail
       
       # Reactive data processing with logging
       data_env <- reactive({
-        message("Evaluating data for measure: ", id)
+        
+        # Select youth or standard calculation expression
+        selected_expr <- if (is_youth) qpr_expr[[paste0(id, "_youth")]]$expr else calculate_expr
         
         # Evaluate the calculation expression
         result <- tryCatch({
-          eval(calculate_expr)
+          eval(selected_expr)
         }, error = function(e) {
           message("Error in data calculation: ", e$message)
           NULL
@@ -91,8 +103,11 @@ mod_body_qpr_server <- function(id, header, calculate_expr, infobox_expr, detail
         req(data_env())  # Ensure data is available
         message("Rendering infobox for measure: ", id)
         
+        # Select youth or standard infobox expression
+        selected_infobox <- if (is_youth) qpr_expr[[paste0(id, "_youth")]]$infobox else infobox_expr
+        
         tryCatch({
-          eval(infobox_expr)
+          eval(selected_infobox)
         }, error = function(e) {
           message("Error rendering infobox: ", e$message)
           NULL
@@ -104,8 +119,11 @@ mod_body_qpr_server <- function(id, header, calculate_expr, infobox_expr, detail
         req(data_env())  # Ensure data is available
         message("Rendering details for measure: ", id)
         
+        # Select youth or standard details expression
+        selected_details <- if (is_youth) qpr_expr[[paste0(id, "_youth")]]$details else details_expr
+        
         tryCatch({
-          eval(details_expr)
+          eval(selected_details)
         }, error = function(e) {
           message("Error rendering details: ", e$message)
           NULL
