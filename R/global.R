6ce537fc0ed58c991c0d4d409a3cbd8092cbf310
Trevin Flickinger <twflick@gmail.com>
2024-09-24 14:35:09 -0400

    remove maleta, dropbox dependencies, new functions to replace

--- R/global.R
+++ R/global.R
@@ -14,11 +14,126 @@
 # loading the image files from the data/ folder
 #' @include golem_utils_server.R
 
+#### functions from yogat3ch/maleta
+#### removed dropbox dependency
+
+#' Clean rds files with NULL values
+#'
+#' @param files \code{(chr)} of files to clean NULL saves
+#'
+#' @return \code{(chr)} of valid files after cleaning
+
+clean_null <- function(files) {
+  .rds <- stringr::str_subset(files, "rds$")
+  .sizes <- file.size(.rds)
+  file.remove(.rds[.sizes == 44])
+  files[!files %in% .rds[.sizes == 44]]
+}
+
+#' Create accessor function for each dependency
+#'
+#' @param .x \code{(chr)} The full path of the file.
+#' @param do_update \code{(lgl)} If TRUE, will update the dependency.
+#' @return A function that can be used to access the dependency.
+accessor_create <- function(.x, do_update) {
+  rlang::new_function(
+    args = rlang::pairlist2(
+      path = rlang::expr(!!.x),
+      dep_update = update_local,  # No more Dropbox, use local update function
+      do_update = rlang::expr(!!do_update),
+      ... = 
+    ),
+    body = base::quote({
+      if (do_update) {
+        dep_update(path)  # Update from local source
+      }
+      UU::file_fn(path)(path, ...)  # Use the file function on the path
+    })
+  )
+}
+
+#' Assign accessor functions to the package namespace
+#'
+#' @param funs A list of accessor functions to assign.
+#' @param ns The package namespace where functions will be assigned.
+#' @return Functions are assigned to the namespace or returned as a list.
+do_assignment <- function(funs, ns = pkgload::pkg_name()) {
+  if (UU::is_legit(try(ns, silent = TRUE))) {
+    namespace <- rlang::ns_env(ns)
+    rlang::env_unlock(namespace)
+    purrr::iwalk(funs, ~{
+      if (exists(.y, envir = namespace, inherits = FALSE)) {
+        rlang::env_binding_unlock(namespace, .y)
+      }
+      assign(.y, .x, envir = namespace)
+      assignInNamespace(.y, .x, ns, envir = namespace)
+      rlang::env_binding_lock(namespace, .y)
+    })
+  } else {
+    funs
+  }
+}
+
+#' Update a dependency locally (no remote)
+#'
+#' @param filepaths \code{(chr)} A list of file paths to check and load.
+#' @return Loaded dependencies.
+update_local <- function(filepaths) {
+  # You can add logic here to check file timestamps or reload the files
+  purrr::walk(filepaths, function(filepath) {
+    message(glue::glue("Checking and loading {filepath}..."))
+    # Example: If file doesn't exist or is outdated, reload it
+    if (!file.exists(filepath)) {
+      stop(glue::glue("File {filepath} not found."))
+    }
+  })
+}
+
+#' Create accessor functions in the namespace (local files only)
+#'
+#' @param dep_dir \code{(chr)} The directory where dependencies are stored.
+#' @param deps \code{(chr)} Full file paths for dependencies to be accessed.
+#' @param dep_update \code{(function)} Dependency update function.
+#' @param update_all \code{(lgl)} Whether to load/update all dependencies.
+#' @return Accessor functions will be available in the package namespace.
+#' @export
+create_accessors <- function(dep_dir = "data", deps = NULL, dep_update = update_local, update_all = TRUE) {
+  # Get the list of dependencies if not provided
+  if (is.null(deps)) {
+    deps <- clean_null(UU::list.files2(dep_dir)) |>
+      stringr::str_subset("\\.png$", negate = TRUE)  # Avoid .png files
+  }
+  
+  # Get absolute paths of dependencies
+  deps <- fs::path_abs(deps)
+  
+  # Ensure the directory exists
+  UU::mkpath(dep_dir)
+  
+  # Optionally update all dependencies
+  if (update_all) {
+    dep_update(deps)
+  }
+  
+  # Create accessor functions for each dependency
+  accessor_funs <- purrr::map(
+    rlang::set_names(deps, fs::path_ext_remove(basename(deps))),
+    accessor_create,
+    do_update = !update_all
+  )
+  
+  # Assign accessor functions to the namespace or return them as a list
+  do_assignment(accessor_funs)
+}
+
+
+
+
 # Create accessor functions
 Sys.setenv(TZ = "America/New_York")
 
 .time <- system.time({
-  maleta::create_accessors("data")
+  create_accessors("data")
 })
 
   
